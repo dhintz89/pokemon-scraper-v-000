a4f140ce98fa19b417f41c61de83c97677857ff8
Dan Hintz <dhintz@live.com>
2018-11-09 02:05:02 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
   
   def self.find(id, db)
     pokemon=db.execute("SELECT * FROM Pokemon WHERE id=?",[id]).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
 
   def alter_hp(new_hp, db)
